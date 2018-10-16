d075aad400d0be66bfad24c58b697c457b52473d
Vempz <Vempz@users.noreply.github.com>
2018-10-15 20:58:33 -0700

    NE-1 optimize Teldrassil guide. (#8)

--- TourGuide_Alliance/01_12_Teldrassil.lua
+++ TourGuide_Alliance/01_12_Teldrassil.lua
@@ -63,11 +63,14 @@ A Dolanaar Delivery |N|South out of Shadowglen (61,47)| |QID|2159|
 A Zenn's Bidding |N|Along the road to Dolannar (60,56)| |QID|488|
 
 R Dolanaar
+B Sell junk/repair and buy arrows
 h Dolanaar
 T Dolanaar Delivery |QID|2159|
 T Crown of the Earth (Part 2) |N|South of the inn by the moonwell| |QID|928|
 A Crown of the Earth (Part 3) |QID|929|
 A Recipe of the Kaldorei |N|Need 1 silver to train cooking first| |QID|4161|
+C Recipe of the Kaldorei |QID|4161|
+T Recipe of the Kaldorei |QID|4161|
 A Denalan's Earth |N|In front of the tower| |QID|997|
 A A Troubling Breeze |N|In the tower| |QID|475|
 A The Emerald Dreamcatcher |N|In the tower| |QID|2438|
@@ -79,6 +82,7 @@ A Gnarlpine Corruption |QID|476|
 C The Emerald Dreamcatcher |N|East of the questgiver in a house is a Dresser (68.01, 59.66)| |QID|2438|
 T Zenn's Bidding |QID|488|
 A Seek Redemption! |N|In front of the tower at Dolanaar| |QID|489|
+T Gnarlpine Corruption |N|top of ramp at entrance to tower| |QID|476|
 N Keep an eye out for Fel Cones |N|Needed for "Seek Redemption!". Found at the bases of trees|
 
 T Denalan's Earth |N|South at the lake (60.90, 68.49)| |QID|997|
@@ -92,42 +96,91 @@ T Timberling Sprouts |QID|919|
 
 T Crown of the Earth (Part 3) |N|Back in Dolanaar| |QID|929|
 A Crown of the Earth (Part 4) |QID|933|
-T Gnarlpine Corruption |N|In the tower| |QID|476|
 T The Emerald Dreamcatcher |QID|2438|
 A Ferocitas the Dream Eater |QID|2459|
 
 C Seek Redemption! |QID|489|
 T Seek Redemption! |N|East at Zenn Foulhoof| |QID|489|
 C Ferocitas the Dream Eater |N|North of Starbreeze. Kill Ferocitas (69.37, 53.41) and open the necklace he drops for the jewel.| |U|8049| |QID|2459|
+H Dolanaar
 
+B Sell junk/repair and buy arrows
+B Train First Aid
 T Ferocitas the Dream Eater |QID|2459|
 
 A The Road to Darnassus |N|From a mounted patrol on the road west of town| |QID|487|
 C The Road to Darnassus |N|Outside of Ban'ethil Hollow (46,52), south of the road where the NPC partrols.| |QID|487|
 
 C Crown of the Earth (Part 4) |N|Moonwell at the Pools of Arlithrien (42.42, 67.08)| |U|5621| |QID|933|
-
-T Crown of the Earth (Part 4) |N|Back at Dolanaar, you can die and spirit rez. Down by the moonwell.| |QID|933|
+A The Glowing Fruit |N|Out of town, south of the Pools of Arlithrien, just outside Gnarlpine Hold (42.61, 76.11)| |QID|930|
+D Die and spirit rez in Dolanaar
+T Crown of the Earth (Part 4) |QID|933|
 A Crown of the Earth (Part 5) |QID|7383|
 T The Road to Darnassus |QID|487|
+
+R Darnassus
+T Rellian Greenspyre |N|At Cenarion Enclave (38.17, 21.65)| |Z|Darnassus| |QID|922|
+A Tumors |QID|923|
+A The Temple of the Moon |N|Behind the bank, near the portal to Rut'theran Village| |QID|2519|
+T The Temple of the Moon |N|Due south of the bank| |QID|2519|
+A Tears of the Moon |QID|2518|
+
+B 2 stacks of meat from the meat vendor
+B repair and buy arrows |N|Make sure you have more than 2 stacks|
+
+A The Enchanted Glade |QID|937|
+C Crown of the Earth (Part 5) |N|Moonwell at The Oracle Glade (38.44, 34.08)| |U|18152| |QID|7383|
+A The Shimmering Frond |N|Slightly northwest of the moonwell (34.7, 28.8)| |QID|931|
+C The Enchanted Glade |QID|937|
+T The Enchanted Glade |QID|937|
+A Teldrassil |QID|940|
+
+C Tumors |N|Elementals at the river east of The Oracle Glade (42,42)| |QID|923|
+C Tears of the Moon |N|North-northeast, near the edge of the tree. Spawns in a few different spots (39,25) (41,25) (47,25)| |QID|2518|
+D Die and spirit rez in Darnassus
+
+T Tumors |N|Back in Cenarion Enclave, should hit level 10| |QID|923|
+A Return to Denalan |QID|2498|
+A The Hunter's Path |QID|6071|
+B Train skills
+T Teldrassil |QID|940|
+A Grove of the Ancients |QID|952|
+T Tears of the Moon |QID|2518|
+A Sathrah's Sacrifice |QID|2520|
+C Sathrah's Sacrifice |N|Use the item down in the fountain| |U|8155| |QID|2520|
+T Sathrah's Sacrifice |QID|2520|
+B Train Staves
+B Sell junk/repair, buy Sharp Arrows, and buy Laminated Recurve Bow
+H Dolanaar
+
+T Crown of the Earth (Part 5) |N|Down by the moonwell again| |QID|7383|
+A Crown of the Earth (Part 6) |QID|935|
 
 A Taming the Beast (Part 1) |C|Hunter| |QID|6063| |R|Night Elf|
 C Taming the Beast (Part 1) |C|Hunter| |N|Tame a webwood lurker (58,60)| |QID|6063| |R|Night Elf|
 T Taming the Beast (Part 1) |C|Hunter| |QID|6063| |R|Night Elf|
 A Taming the Beast (Part 2) |C|Hunter| |QID|6101| |R|Night Elf|
-C Taming the Beast (Part 2) |C|Hunter| |N|Tame a nightsaber stalker (55,73)| |QID|6101| |R|Night Elf|
+C Taming the Beast (Part 2) |C|Hunter| |N|Tame a nightsaber stalker south of Lake Al'Ameth| |QID|6101| |R|Night Elf|
+D Die and spirit rez in Dolanaar
 T Taming the Beast (Part 2) |C|Hunter| |QID|6101| |R|Night Elf|
 A Taming the Beast (Part 3) |C|Hunter| |QID|6102| |R|Night Elf|
-C Taming the Beast (Part 3) |C|Hunter| |N|Tame a strigid screecher (55,73)| |QID|6102| |R|Night Elf|
+C Taming the Beast (Part 3) |C|Hunter| |N|Tame a strigid screecher south of Lake Al'Ameth| |QID|6102| |R|Night Elf|
+D Die and spirit rez in Dolanaar
 T Taming the Beast (Part 3) |C|Hunter| |QID|6102| |R|Night Elf|
+A Training the Beast |C|Hunter| |QID|6103| |R|Night Elf|
+
+T Return to Denalan |N|South at the lake (60.90, 68.49)| |QID|2498|
+T The Glowing Fruit |QID|930|
+T The Shimmering Frond |QID|931|
 
 A Heeding the Call |C|Druid| |QID|5923| |R|Night Elf|
 
-R Darnassus
-T Rellian Greenspyre |N|At Cenarion Enclave (38.17, 21.65)| |Z|Darnassus| |QID|922|
-A Tumors |QID|923|
-
+R Darnassus |N|Tame a Strigid Hunter on the way and learn Claw rank 2, then tame an Elder Nightsaber|
+A Nessa Shadowsong |N|From Mydrannul (70.67, 45.37)| |R|Night Elf| |Z|Darnassus| |QID|6344|
 T Training the Beast |C|Hunter| |N|(40,8)| |QID|6103| |R|Night Elf|
+N immediately feed your pet
+B Train pet skills
+T Crown of the Earth (Part 6) |QID|935|
 
 T Heeding the Call |C|Druid| |N|Northwest in Cenarion Enclave (33,8)| |Z|Darnassus| |QID|5923| |R|Night Elf|
 A Moonglade |C|Druid| |QID|5921| |R|Night Elf|
@@ -143,44 +196,6 @@ F Auberdine |C|Druid| |R|Night Elf|
 C Body and Heart |N|The cave west of Auberdine. You might want to ask someone to help you to complete this! (43.50,45.97)| |C|Druid| |Z|Darkshore| |QID|6001| |R|Night Elf|
 F Darnassus |C|Druid| |R|Night Elf|
 T Body and Heart |C|Druid| |QID|6001| |R|Night Elf|
-
-A The Temple of the Moon |N|Behind the bank, near the portal to Rut'theran Village| |QID|2519|
-T The Temple of the Moon |N|Due south of the bank| |QID|2519|
-A Tears of the Moon |QID|2518|
-
-A Nessa Shadowsong |N|From Mydrannul (70.67, 45.37) on your way out of Darnassus.| |R|Night Elf| |Z|Darnassus| |QID|6344|
-
-A The Glowing Fruit |N|Out of town, south of the Pools of Arlithrien, just outside Gnarlpine Hold (42.61, 76.11)| |QID|930|
-
-C Crown of the Earth (Part 5) |N|Moonwell at The Oracle Glade (38.44, 34.08)| |U|18152| |QID|7383|
-A The Enchanted Glade |QID|937|
-
-A The Shimmering Frond |N|Slightly northwest of the moonwell (34.7, 28.8)| |QID|931|
-C The Enchanted Glade |QID|937|
-T The Enchanted Glade |QID|937|
-A Teldrassil |QID|940|
-
-C Tumors |N|Elementals at the river east of The Oracle Glade (42,42)| |QID|923|
-C Tears of the Moon |N|North-northeast, near the edge of the tree. Spawns in a few different spots (39,25) (41,25) (47,25)| |QID|2518|
-
-T Tumors |N|Back in Cenarion Enclave, Darnassus| |QID|923|
-A Return to Denalan |QID|2498|
-T Tears of the Moon |QID|2518|
-A Sathrah's Sacrifice |QID|2520|
-C Sathrah's Sacrifice |N|Use the item down in the fountain| |U|8155| |QID|2520|
-T Sathrah's Sacrifice |QID|2520|
-
-H Dolanaar
-T Crown of the Earth (Part 5) |N|Down by the moonwell again| |QID|7383|
-A Crown of the Earth (Part 6) |QID|935|
-
-T Return to Denalan |N|South at the lake (60.90, 68.49)| |QID|2498|
-T The Glowing Fruit |QID|930|
-T The Shimmering Frond |QID|931|
-
-T Crown of the Earth (Part 6) |N|Turn it at Cenarion Enclave, Darnassus, at the top of the tower| |QID|935|
-T Teldrassil |QID|940|
-A Grove of the Ancients |QID|952|
 
 R Rut'theran Village |N|Run thru the portal west of the bank (30.52, 41.44)| |Z|Teldrassil| |R|Night Elf|
 T Nessa Shadowsong |R|Night Elf| |QID|6344|
