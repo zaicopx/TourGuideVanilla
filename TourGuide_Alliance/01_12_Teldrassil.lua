e5c4271cb657ec3134a7665540fb9a3870cf7cf3
Vempz <jamie.atyeo@live.com>
2019-05-13 16:05:20 -0700

    NE-2 refine the route further to avoid overshooting level 10.

--- TourGuide_Alliance/01_12_Teldrassil.lua
+++ TourGuide_Alliance/01_12_Teldrassil.lua
@@ -2,8 +2,9 @@ TourGuide:RegisterGuide("Teldrassil (1-12)", "Darkshore (12-18)", "Alliance", fu
 return [[
 A The Balance of Nature (Part 1) |QID|456|
 A A Good Friend |N|Start killing boars and cats for The Balance of Nature on the way to accept this quest| |QID|4495|
+C The Balance of Nature (Part 1) |QID|456|
 A The Woodland Protector (Part 1) |QID|458|
-C The Balance of Nature (Part 1) |N|Should hit level 2| |QID|456|
+B sell trash & buy arrows
 T The Balance of Nature (Part 1) |QID|456|
 A The Balance of Nature (Part 2) |QID|457|
 
@@ -18,10 +19,10 @@ T A Good Friend |N|Kill boars, cats, and spiders along the way| |QID|4495|
 A A Friend in Need |QID|3519|
 D spirit rez
 
+T The Balance of Nature (Part 2) |QID|457|
 T A Friend in Need |QID|3519|
 A Iverron's Antidote (Part 1) |QID|3521|
 B [Hornwood Recurve Bow] |L|2506|
-T The Balance of Nature (Part 2) |QID|457|
 T The Woodland Protector (Part 1) |N|Dryad to the southwest (57.77, 45.20)| |QID|458|
 A The Woodland Protector (Part 2) |QID|459|
 A Webwood Venom |N|At the base of the ramp (57.79, 41.70)| |QID|916|
@@ -31,13 +32,13 @@ C Iverron's Antidote (Part 1) |N|start with the grellkin, you should complete th
 C Webwood Venom |QID|916|
 D spirit rez
 
+T The Woodland Protector (Part 2) |QID|459|
 T Webwood Venom |QID|916|
 A Webwood Egg |QID|917|
 
 C Webwood Egg |N|Inside the cave (56,26)| |QID|917|
 D Die and rez at spirit healer
 
-T The Woodland Protector (Part 2) |QID|459|
 B sell trash
 T Iverron's Antidote (Part 1) |N|Should hit level 5| |QID|3521|
 A Iverron's Antidote (Part 2) |QID|3522|
@@ -48,8 +49,12 @@ T Tenaron's Summons |N|Up the ramp| |QID|920|
 A Crown of the Earth (Part 1) |QID|921|
 C Crown of the Earth (Part 1) |N|Moonwell north of Aldrassil (59.93, 33.07)| |U|5185| |QID|921|
 T Iverron's Antidote (Part 2) |QID|3522|
+N make sure you are <= 380 XP from level 6
 
 H Hearth back to start
+
+T Crown of the Earth (Part 1) |QID|921|
+A Crown of the Earth (Part 2) |QID|928|
 
 T Encrypted Sigil |C|Rogue| |QID|3118| |R|Night Elf|
 T Etched Sigil |C|Hunter| |N|Up the ramp!| |QID|3117| |R|Night Elf|
@@ -57,9 +62,6 @@ T Hallowed Sigil |C|Priest| |QID|3119| |R|Night Elf|
 T Simple Sigil |C|Warrior| |QID|3116| |R|Night Elf|
 T Verdant Sigil |C|Druid| |N|Up the ramp!| |QID|3120| |R|Night Elf|
 B train skills
-
-T Crown of the Earth (Part 1) |QID|921|
-A Crown of the Earth (Part 2) |QID|928|
 
 A Dolanaar Delivery |N|South out of Shadowglen (61,47)| |QID|2159|
 A Zenn's Bidding |N|Along the road to Dolannar (60,56)| |QID|488|
@@ -70,10 +72,10 @@ h Dolanaar
 T Crown of the Earth (Part 2) |N|South of the inn by the moonwell| |QID|928|
 A Crown of the Earth (Part 3) |QID|929|
 A Recipe of the Kaldorei |N|Need 1 silver to train cooking first| |QID|4161|
-B train skills
 A Denalan's Earth |N|In front of the tower| |QID|997|
 A A Troubling Breeze |N|In the tower| |QID|475|
 A The Emerald Dreamcatcher |N|In the tower| |QID|2438|
+A Twisted Hatred |QID|932|
 
 T Denalan's Earth |N|South at the lake (60.90, 68.49)| |QID|997|
 A Timberling Seeds |QID|918|
@@ -83,24 +85,31 @@ C Timberling Sprouts |QID|919|
 T Timberling Seeds |QID|918|
 A Rellian Greenspyre |QID|922|
 
-C Crown of the Earth (Part 3) |N|Moonwell is East of Dolanaar (63.37, 58.12)| |U|5619| |QID|929|
-C Zenn's Bidding |N|East of Dolanaar| |QID|488|
+C The Emerald Dreamcatcher |N|East of the questgiver in a house is a Dresser (68.01, 59.66)| |QID|2438|
 T A Troubling Breeze |N|East in Starbreeze Village (66,58)| |QID|475|
 A Gnarlpine Corruption |QID|476|
-C The Emerald Dreamcatcher |N|East of the questgiver in a house is a Dresser (68.01, 59.66)| |QID|2438|
-
+C Crown of the Earth (Part 3) |U|5619| |QID|929|
+C Zenn's Bidding |N|East of Dolanaar| |QID|488|
 T Zenn's Bidding |QID|488|
+
+C Twisted Hatred |QID|932|
+D RUN OUT OF THE CAVE BEFORE YOU DIE OR YOU WILL SPAWN IN SHADOWGLEN
+
+T Crown of the Earth (Part 3) |N|Back in Dolanaar| |QID|929|
+A Crown of the Earth (Part 4) |QID|933|
+B sell trash
+
 A Seek Redemption! |N|In front of the tower at Dolanaar| |QID|489|
 T The Emerald Dreamcatcher |QID|2438|
 A Ferocitas the Dream Eater |QID|2459|
 
 C Seek Redemption! |QID|489|
+T Seek Redemption! |N|East at Zenn Foulhoof| |QID|489|
 C Recipe of the Kaldorei |QID|4161|
 C Ferocitas the Dream Eater |N|North of Starbreeze. Kill Ferocitas (69.37, 53.41) and open the necklace he drops for the jewel.| |U|8049| |QID|2459|
 D spirit rez in Dolanaar
 
-T Crown of the Earth (Part 3) |N|Back in Dolanaar| |QID|929|
-A Crown of the Earth (Part 4) |QID|933|
+B sell trash and buy arrows if necessary
 A The Road to Darnassus |N|From a mounted patrol on the road west of town| |QID|487|
 C The Road to Darnassus |N|Outside of Ban'ethil Hollow (46,52), south of the road where the NPC partrols.| |QID|487|
 
@@ -113,37 +122,16 @@ A Crown of the Earth (Part 5) |QID|7383|
 R just west just past the bridge
 D spirit rez in Darnassus
 T Rellian Greenspyre |N|At Cenarion Enclave (38.17, 21.65)| |Z|Darnassus| |QID|922|
-A Tumors |QID|923|
-A The Temple of the Moon |N|Behind the bank, near the portal to Rut'theran Village| |QID|2519|
-T The Temple of the Moon |N|Due south of the bank| |QID|2519|
-A Tears of the Moon |QID|2518|
-B 2 stacks of meat from the meat vendor
-B sell trash and buy arrows |N|Make sure you have more than 2 stacks|
+B train skills
 
 R out of Darnassus and then north to The Oracle Glade
 A The Enchanted Glade |QID|937|
 C Crown of the Earth (Part 5) |N|Moonwell at The Oracle Glade (38.44, 34.08)| |U|18152| |QID|7383|
 A The Shimmering Frond |N|Slightly northwest of the moonwell (34.7, 28.8)| |QID|931|
-C The Enchanted Glade |QID|937|
-T The Enchanted Glade |QID|937|
-A Teldrassil |QID|940|
-
-C Tumors |N|Elementals at the river east of The Oracle Glade (42,42)| |QID|923|
-C Tears of the Moon |N|North-northeast, near the edge of the tree. Spawns in a few different spots (39,25) (41,25) (47,25)| |QID|2518|
-D Die and spirit rez in Darnassus
-
-T Tumors |N|Back in Cenarion Enclave| |QID|923|
-A Return to Denalan |QID|2498|
-A Grove of the Ancients |QID|952|
-T Tears of the Moon |QID|2518|
-A Sathrah's Sacrifice |QID|2520|
-C Sathrah's Sacrifice |N|Use the item down in the fountain| |U|8155| |QID|2520|
-T Sathrah's Sacrifice |QID|2520|
 H Dolanaar
 
 T Dolanaar Delivery |QID|2159|
 T Timberling Sprouts |N|South at the lake (60.90, 68.49)| |QID|919|
-T Return to Denalan |QID|2498|
 T The Glowing Fruit |QID|930|
 T The Shimmering Frond |QID|931|
 
@@ -152,8 +140,8 @@ D spirit rez in Dolanaar
 T The Road to Darnassus |QID|487|
 T Gnarlpine Corruption |N|top of ramp at entrance to tower| |QID|476|
 T Ferocitas the Dream Eater |QID|2459|
+T Twisted Hatred |QID|932|
 T Recipe of the Kaldorei |QID|4161|
-T Seek Redemption! |N|East at Zenn Foulhoof| |QID|489|
 T Crown of the Earth (Part 5) |N|Down by the moonwell again| |QID|7383|
 A Crown of the Earth (Part 6) |QID|935|
 
@@ -176,7 +164,6 @@ T Training the Beast |C|Hunter| |N|(40,8)| |QID|6103| |R|Night Elf|
 N immediately feed your pet
 B Train pet skills
 T Crown of the Earth (Part 6) |QID|935|
-T Teldrassil |QID|940|
 
 R Rut'theran Village |N|Run thru the portal west of the bank (30.52, 41.44)| |Z|Teldrassil| |R|Night Elf|
 T Nessa Shadowsong |R|Night Elf| |QID|6344|
